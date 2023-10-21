f56a7cbe6e1b0988b459ee960f582ebfeb96c4b0
Ayush <ayushsajwan19@gmail.com>
2023-10-21 23:34:43 +0530

    Added Functionality to remove old photo when user updates profile photo

--- routes/updateProfilePhoto.js
+++ routes/updateProfilePhoto.js
@@ -1,16 +1,27 @@
 const express=require('express');
 const User=require("../schemas/userSchema");
 const router=express.Router();
+const fs=require('fs');
+const path = require('path');
 
 router.route("/:id")
 .post(async (req,res)=>{
 
     try{
 
+        const user=await User.findById({_id:req.params.id});
+
+        // const path='./public/uploads/'+user.profileImage;
+        const filePath = path.join(__dirname, '..', 'public','uploads', user.profileImage);
+
+
+        fs.unlinkSync(filePath);
+        
         await User.updateOne(
             {_id:req.params.id},
             {$set:{profileImage:req.file.filename}}
         );
+
     }
     catch(err){
         console.log(err);
